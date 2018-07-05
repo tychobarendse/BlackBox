6fab4e9c77e9f07f964f164751c9cb044d65aa51
tychobarendse <tychobarendse@gmail.com>
2018-07-05 10:22:12 +0200

    Update blacklcd.py

--- blacklcd.py
+++ blacklcd.py
@@ -6,8 +6,7 @@ from luma.lcd.device import pcd8544, st7735, uc1701x
 
 serial = spi(port=32766, device=0, gpio=CHIP_IO.GPIO, gpio_DC="CSID1", gpio_RST="CSID0")
 device = ss7735(serial)
-with canvas(device) as draw:
- draw.rectangle(device.bounding_box, outline="white")
- draw.text((3, 3), "Hello", fill="white")
- 
+ with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), "Hello World", fill="red")
  time.sleep(10)
