274dae9ad6e9531a7ad81371a6fcf0fc4b35e1dd
tychobarendse <tychobarendse@gmail.com>
2018-07-05 21:57:26 +0200

    Update blacklcd.py

--- blacklcd.py
+++ blacklcd.py
@@ -13,10 +13,11 @@ device = st7735(serial)
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
     draw.text((30, 40), "Hello World", fill="red")
-time.sleep(10)
+time.sleep(1)
  
 # open photo
-photo = Image.open(img_path).convert("RGBA")
+photo = Image.open(img_path)#.convert("RGBA
+photo = Image.open(img_path).transform(device.size, Image.AFFINE, (1, 0, 0, 0, 1, 0), Image.BILINEAR).convert(device.mode)
 # display on screen for a few seconds
-device.display(photo.convert(device.mode))
+device.display(photo) #.convert(device.mode))
 time.sleep(10)
